48dfc6d522ce4e78eb5f15f69d33ee3a919260b6
CodeFuller <CodeFuller@gmail.com>
2021-05-01 22:12:24 +0300

    Add functional test for storing and retrieving complex object

---
+++ tests/RedisDemo.FunctionalTests/ObjectTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackExchange.Redis.Extensions.Core.Abstractions;
+using StackExchange.Redis.Extensions.Core.Configuration;
+using StackExchange.Redis.Extensions.Newtonsoft;
+
+namespace RedisDemo.FunctionalTests
+{
+	[TestClass]
+	public class ObjectTests
+	{
+		private class NestedObject
+		{
+			public decimal DecimalProperty { get; init; }
+		}
+
+		private class SomeObject
+		{
+			public string StringProperty { get; init; }
+
+			public int NumericProperty { get; init; }
+
+			public NestedObject NestedObject { get; init; }
+		}
+
+		[TestMethod]
+		public async Task GetObject_ForExistingObject_ReturnsCorrectValue()
+		{
+			// Arrange
+
+			var redisConfiguration = new RedisConfiguration
+			{
+				KeyPrefix = "RedisDemo.FunctionalTests",
+				Hosts = new[]
+				{
+					new RedisHost { Host = "localhost", Port = 6379 },
+				},
+			};
+
+			var services = new ServiceCollection();
+			services.AddStackExchangeRedisExtensions<NewtonsoftSerializer>(redisConfiguration);
+
+			await using var serviceProvider = services.BuildServiceProvider();
+			var cacheClient = serviceProvider.GetRequiredService<IRedisCacheClient>();
+
+			var originalObject = new SomeObject
+			{
+				StringProperty = "Some String",
+				NumericProperty = 12345,
+				NestedObject = new NestedObject
+				{
+					DecimalProperty = 123.456789M,
+				},
+			};
+
+			// Act
+
+			var wasSet = await cacheClient.Db0.AddAsync("Some Object", originalObject);
+			Assert.IsTrue(wasSet);
+
+			var objectFromCache = await cacheClient.Db0.GetAsync<SomeObject>("Some Object");
+
+			// Assert
+
+			objectFromCache.Should().NotBeSameAs(originalObject);
+			objectFromCache.Should().BeEquivalentTo(originalObject);
+		}
+	}
+}
