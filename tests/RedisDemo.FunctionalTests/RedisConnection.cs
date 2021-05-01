e6a21d1a0cfa8a784586a983bc1f064cf78f435d
CodeFuller <CodeFuller@gmail.com>
2021-05-01 21:19:38 +0300

    Add functional tests for string commands

---
+++ tests/RedisDemo.FunctionalTests/RedisConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using StackExchange.Redis;
+
+namespace RedisDemo.FunctionalTests
+{
+	public static class RedisConnection
+	{
+		private static readonly Lazy<ConnectionMultiplexer> LazyConnection = new(GetConnection, LazyThreadSafetyMode.ExecutionAndPublication);
+
+		public static ConnectionMultiplexer Redis => LazyConnection.Value;
+
+		private static ConnectionMultiplexer GetConnection()
+		{
+			return ConnectionMultiplexer.Connect("localhost");
+		}
+	}
+}
---
+++ tests/RedisDemo.FunctionalTests/StringTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static RedisDemo.FunctionalTests.RedisConnection;
+
+namespace RedisDemo.FunctionalTests
+{
+	[TestClass]
+	public class StringTests
+	{
+		[TestMethod]
+		public async Task StringGetAsync_ForMissingKey_ReturnsNull()
+		{
+			// Arrange
+
+			var database = Redis.GetDatabase();
+
+			// Act
+
+			var value = await database.StringGetAsync("RedisDemo.FunctionalTests.StringTests.Missing Key");
+
+			// Assert
+
+			value.IsNull.Should().BeTrue();
+		}
+
+		[TestMethod]
+		public async Task StringGetAsync_ForExistingKey_ReturnsCorrectValue()
+		{
+			// Arrange
+
+			var database = Redis.GetDatabase();
+
+			var wasSet = await database.StringSetAsync("RedisDemo.FunctionalTests.StringTests.Existing Key", "Some Value");
+			Assert.IsTrue(wasSet);
+
+			// Act
+
+			string value = await database.StringGetAsync("RedisDemo.FunctionalTests.StringTests.Existing Key");
+
+			// Assert
+
+			value.Should().Be("Some Value");
+		}
+	}
+}
